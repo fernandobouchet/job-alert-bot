611723c6d410ba3abcd2386d9303ca48fe7c071a
Fernando Bouchet <fernandobouchet@gmail.com>
2025-11-06 00:34:58 -0300

    feat: update 'filters_scoring_config'

--- filters_scoring_config/patterns.py
+++ filters_scoring_config/patterns.py
@@ -5,4 +5,5 @@ SENIOR_EXPERIENCE_PATTERNS = [
     r"(?:(\d+)\+?|\+(\d+))\s*(?:a[ñn]os|years?)\s*(?:de\s*|of\s*)*experiencias?",
     r"(?:experiencia|experience)\s*(?:de|of)\s*(?:(\d+)\+?|\+(\d+))\s*(?:a[ñn]os|years?)",
     r"for\s*(?:(\d+)\+?|\+(\d+))\s*years?",
+    r"(?:(\d+)\\\+?|\\\+(\d+))\s*(?:a[ñn]os|years?)",
 ]
