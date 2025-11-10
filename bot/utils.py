bc6018fb57daf8271fbb77489d8c5790723251ab
Fernando Bouchet <fernandobouchet@gmail.com>
2025-11-10 19:08:59 -0300

    feat: update 'bot/utils.py'

--- bot/utils.py
+++ bot/utils.py
@@ -1,6 +1,6 @@
 import asyncio
 import re
-from telegram import constants
+from telegram import constants, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 async def send_jobs(bot, channel_id, jobs):
@@ -13,16 +13,22 @@ async def send_jobs(bot, channel_id, jobs):
         tags_display = ", ".join(sorted(list(set(tags_list))))
 
         text = (
-            f"💼 <b>{clean_text(job.get('title', 'N/A'))}</b> ({clean_text(job.get('modality', 'N/A'))})\n"
-            f"--- \n"
+            f"🌐 Fuente: <b>{clean_text(job.get('source', 'N/A'))}</b>\n"
+            f"💼 <b>{clean_text(job.get('title', 'N/A'))}</b>\n"
+            f"🧭 Modalidad: {clean_text(job.get('modality', 'N/A'))}\n"
             f"🏢 Empresa: {clean_text(job.get('company', 'N/A'))}\n"
-            f"💰 Salario: {clean_text(job.get('salary', 'No especificado'))}\n"
-            f"🏷️ Tags: <code>{tags_display}</code>\n\n"
-            f"🆔 ID: <code>{job.get('id', 'N/A')}</code>\n"
-            f"🏢 Fuente: {clean_text(job.get('source', 'N/A'))}\n"
-            f"📝 Descripción:\n"
-            f"{clean_text(job.get('description', ''))[:200]}...\n\n"
-            f"🔗 <a href='{clean_text(job.get('url', '#'))}'>Ver detalles</a>"
+            f"🏷️ Tags: <code>{tags_display}</code>\n"
+        )
+
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text=f"🔗 Ver detalles en {clean_text(job.get('source', 'N/A'))}",
+                        url=clean_text(job.get("url", "#")),
+                    )
+                ]
+            ]
         )
 
         try:
@@ -31,6 +37,7 @@ async def send_jobs(bot, channel_id, jobs):
                 text=text,
                 parse_mode=constants.ParseMode.HTML,
                 disable_web_page_preview=True,
+                reply_markup=keyboard,
             )
             await asyncio.sleep(1.0)
         except Exception as e:
